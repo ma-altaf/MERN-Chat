fd1226e2f46573033a45242fb99824cae8b29abd
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2022-07-04 16:07:50 -0300

    Initial set up
    - Created separate folder for client and server side
    - Changed create-react-app for the initial setup on the front-end

---
+++ client/messaging-app/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+);
---
+++ client/messaging-app/src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+    return <div className="text-3xl">Hello world</div>;
+}
+
+export default Home;
