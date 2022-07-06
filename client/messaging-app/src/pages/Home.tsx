671aebd2ae1734d74b115b63b52affe38da4fb26
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2022-07-05 21:10:43 -0300

    Rename front-end folder + homepage layout

---
+++ client/messaging-app/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <div className="grid grid-cols-3 h-screen w-screen">
+            {/* description */}
+            <div className="bg-primary h-full p-4">dfds</div>
+            {/* login form */}
+            <div className="col-span-2 h-full p-4   ">dfsf</div>
+        </div>
+    );
+}
+
+export default Home;
--- client/my-app/src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Home() {
-    return <div className="text-3xl">Hello World</div>;
-}
-
-export default Home;
