1ca226a849b63f6e8a0aee1a3c1287a8729928b1
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2022-07-06 19:50:20 -0300

    Updated the form UI

--- client/messaging-app/tailwind.config.js
+++ client/messaging-app/tailwind.config.js
@@ -2,13 +2,6 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        colors: {
-            primary: "#1D7874",
-            secondary: "#679289",
-            accent: "#F4C095",
-            danger: "#EE2E31",
-            warning: "#1D7874",
-        },
         extend: {},
     },
     plugins: [],
