b29b78f96cca6b7da81edc395ff8c1943ee85eab
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2022-08-06 19:25:08 -0700

    Refactor edit profile panel + UI change to profile panel

---
+++ client/messaging-app/src/components/EditProfile.tsx
@@ -0,0 +1,107 @@
+import { useEffect, useState } from "react";
+import { User } from "../context/AuthContext";
+import apiFetch from "../utils/apiFetch";
+
+type Props = {
+    user: User | undefined;
+    setUser: React.Dispatch<React.SetStateAction<User>> | undefined;
+    setIsEditProfile: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+function EditProfile({ user, setUser, setIsEditProfile }: Props) {
+    const [newUsername, setNewUsername] = useState("");
+    const [newAbout, setNewAbout] = useState("");
+
+    useEffect(() => {
+        setNewUsername(user?.username || "");
+        setNewAbout(user?.about || "");
+
+        return;
+    }, [user]);
+
+    const changeName = async () => {
+        const res = await apiFetch("/users/changeUsername", "POST", {
+            newUsername,
+        });
+
+        if (res.ok) {
+            setUser?.((prev) => ({ ...prev, username: newUsername }));
+            setIsEditProfile(false);
+        } else {
+            setNewUsername(user?.username || "");
+            window.alert(
+                "Username could not be changed. The username might not be available."
+            );
+        }
+    };
+
+    const changeAbout = async () => {
+        const res = await apiFetch("/users/changeAbout", "POST", {
+            newAbout,
+        });
+
+        if (res.ok) {
+            setUser?.((prev) => ({ ...prev, about: newAbout }));
+            setIsEditProfile(false);
+        } else {
+            setNewAbout(user?.about || "");
+            window.alert("About could not be changed.");
+        }
+    };
+
+    return (
+        <div
+            className="fixed top-0 left-0 flex justify-center items-center z-50 w-screen h-screen m-0 p-0 bg-black bg-opacity-80"
+            onClick={() => setIsEditProfile(false)}
+        >
+            <div
+                className="flex flex-col justify-center items-start rounded-lg max-w-[90%] md:max-w-1/2 h-fit bg-white p-4"
+                onClick={(e) => e.stopPropagation()}
+            >
+                <span className="p-1">
+                    <label htmlFor="newUsername">Name:</label>
+                    <input
+                        type="text"
+                        name="newUsername"
+                        id="newUsername"
+                        value={newUsername}
+                        onChange={(e) => setNewUsername(e.target.value)}
+                        className="border-2 rounded-lg p-1 mx-1 focus:border-black"
+                    />
+                    <button
+                        className="bg-black rounded-lg text-white px-2 py-1 my-1"
+                        onClick={changeName}
+                    >
+                        Rename
+                    </button>
+                </span>
+                <span className="p-1 flex flex-col">
+                    <label htmlFor="newAbout">About:</label>
+                    <textarea
+                        className="border-2 rounded-lg p-2 my-1 focus:border-black w-full"
+                        name="newAbout"
+                        id="newAbout"
+                        cols={80}
+                        rows={10}
+                        onChange={(e) => setNewAbout(e.target.value)}
+                        value={newAbout}
+                    />
+                    <button
+                        className="bg-black rounded-lg text-white px-2 py-1"
+                        onClick={changeAbout}
+                    >
+                        Change About
+                    </button>
+                    <button
+                        className="border-black border-2 rounded-lg mt-1"
+                        onClick={() => setIsEditProfile(false)}
+                    >
+                        Close
+                    </button>
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default EditProfile;
--- client/messaging-app/src/components/UserBanner.tsx
+++ client/messaging-app/src/components/UserBanner.tsx
@@ -4,6 +4,7 @@ import EditableProfileImg from "./EditableProfileImg";
 import apiFetch from "../utils/apiFetch";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import EditProfile from "./EditProfile";
 
 type Props = {
     user: User | undefined;
@@ -12,15 +13,6 @@ type Props = {
 function UserBanner({ user, setUser }: Props) {
     const navigate = useNavigate();
     const [isEditProfile, setIsEditProfile] = useState(false);
-    const [newUsername, setNewUsername] = useState("");
-    const [newAbout, setNewAbout] = useState("");
-
-    useEffect(() => {
-        setNewUsername(user?.username || "");
-        setNewAbout(user?.about || "");
-
-        return;
-    }, [user]);
 
     const logOut = async () => {
         if (window.confirm("Confirm log out")) {
@@ -35,80 +27,14 @@ function UserBanner({ user, setUser }: Props) {
         }
     };
 
-    const changeName = async () => {
-        const res = await apiFetch("/users/changeUsername", "POST", {
-            newUsername,
-        });
-
-        if (res.ok) {
-            setUser?.((prev) => ({ ...prev, username: newUsername }));
-            setIsEditProfile(false);
-        } else {
-            setNewUsername(user?.username || "");
-            window.alert("Username could not be changed.");
-        }
-    };
-
-    const changeAbout = async () => {
-        const res = await apiFetch("/users/changeAbout", "POST", {
-            newAbout,
-        });
-
-        if (res.ok) {
-            setUser?.((prev) => ({ ...prev, about: newAbout }));
-            setIsEditProfile(false);
-        } else {
-            setNewAbout(user?.about || "");
-            window.alert("About could not be changed.");
-        }
-    };
-
     return (
         <div className="w-full lg:w-1/2 rounded-lg p-8 shadow bg-white flex justify-center items-center flex-col lg:flex-row relative">
             {isEditProfile && (
-                <div className="fixed flex flex-col justify-center items-start z-50 rounded-lg top-1/2 md:left-1/2 md:-translate-x-1/2 -translate-y-1/2 shadow-2xl md:max-w-1/2 h-fit bg-white p-4">
-                    <span className="p-1">
-                        <label htmlFor="newUsername">Name:</label>
-                        <input
-                            type="text"
-                            name="newUsername"
-                            id="newUsername"
-                            value={newUsername}
-                            onChange={(e) => setNewUsername(e.target.value)}
-                            className="border-2 rounded-lg p-1 mx-1 focus:border-black"
-                        />
-                        <button
-                            className="bg-black rounded-lg text-white px-2 p-1"
-                            onClick={changeName}
-                        >
-                            Save
-                        </button>
-                    </span>
-                    <span className="p-1 flex flex-col">
-                        <label htmlFor="newAbout">About:</label>
-                        <textarea
-                            className="border-2 rounded-lg p-2 my-1 focus:border-black"
-                            name="newAbout"
-                            id="newAbout"
-                            cols={35}
-                            rows={10}
-                            onChange={(e) => setNewAbout(e.target.value)}
-                            value={newAbout}
-                        />
-                        <button
-                            className="bg-black rounded-lg text-white px-2"
-                            onClick={changeAbout}
-                        >
-                            Save
-                        </button>
-                        <button
-                            className="border-black border-2 rounded-lg mt-1"
-                            onClick={() => setIsEditProfile(false)}
-                        >
-                            Cancel
-                        </button>
-                    </span>
-                </div>
+                <EditProfile
+                    user={user}
+                    setUser={setUser}
+                    setIsEditProfile={setIsEditProfile}
+                />
             )}
             <span className="absolute top-0 right-0 m-1 p-1 text-2xl">
                 <button onClick={() => setIsEditProfile(true)}>
