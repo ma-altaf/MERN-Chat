9f07bda5348465ba1d398d2349729d42958c8134
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2022-08-11 15:45:36 -0700

    Update RoomList.tsx

--- client/messaging-app/src/pages/Home/components/RoomList.tsx
+++ client/messaging-app/src/pages/Home/components/RoomList.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 function RoomList({ rooms }: Props) {
     return (
-        <div className="w-full h-fit py-6 grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:px-10">
+        <div className="w-full h-fit py-6 grid gap-4 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 lg:px-10">
             {rooms &&
                 rooms.map((room: Room) => (
                     <RoomCard room={room} key={room.roomID} />
